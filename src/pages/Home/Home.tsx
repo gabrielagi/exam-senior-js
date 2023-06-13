81a2b7e360ea9ccb0f0a3a0c04137cd60438c50f
gabrielagi <gabrielagi503@gmail.com>
2023-06-13 19:17:37 -0300

    Nabvar + Datagrid'

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
+import { People } from "@/data/people";
 
 export type HomeProps = {};
+const pagesize = 5;
+const columns = [
+  {
+    field: "name",
+    headerName: "Name",
+    flex: 1,
+    minWidth: 150,
+    renderCell: (params: GridRenderCellParams) => <>{params.field}</>,
+  },
+];
 
 const Home: React.FC<HomeProps> = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <DataGrid
+        rows={People}
+        columns={columns}
+        disableColumnSelector
+        disableRowSelectionOnClick
+        autoHeight
+        rowHeight={pagesize}
+        //pageSize={pagesize}
+        //rowsPerPageOptions={[pagesize]}
+      />
+    </div>
+  );
 };
 
 export default Home;
