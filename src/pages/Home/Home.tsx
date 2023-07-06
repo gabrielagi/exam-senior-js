64eb98cfc9e6fd07204148fe73c53324d97fe61d
gabrielagi <gabrielagi503@gmail.com>
2023-07-06 11:26:10 -0300

    Grid-Layout-Inf

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,14 +3,26 @@ import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
 import { People } from "@/data/people";
 
 export type HomeProps = {};
-const pagesize = 5;
+//const pagesize = 5;
 const columns = [
   {
     field: "name",
     headerName: "Name",
     flex: 1,
     minWidth: 150,
-    renderCell: (params: GridRenderCellParams) => <>{params.field}</>,
+    renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+  },
+  {
+    field: "category",
+    headerName: "Categories",
+    flex: 1,
+    renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+  },
+  {
+    field: "company",
+    headerName: "Company",
+    flex: 1,
+    renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
   },
 ];
 
@@ -27,6 +39,7 @@ const Home: React.FC<HomeProps> = () => {
           pagination: { paginationModel: { pageSize: 5 } },
         }}
         pageSizeOptions={[5, 10, 25]}
+        getRowId={(row: any) => row.id}
       />
     </div>
   );
