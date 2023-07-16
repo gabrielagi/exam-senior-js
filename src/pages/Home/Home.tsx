c2f4675801a8d6a33b3ddb1648ea4689b24f58e9
gabrielagi <gabrielagi503@gmail.com>
2023-07-16 12:45:41 -0300

    Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,54 +4,53 @@ import { People } from "@/data/people";
 import { Person } from "@/models";
 import { Checkbox } from "@mui/material";
 
-export type HomeProps = {};
-//const pagesize = 5;
-const findPerson = (person: Person) =>
-  !!selectedPeople.find((p) => p.id === person.id);
-const filterPerson = (person: Person) =>
-  selectedPeople.filter((p) => p.id === person.id);
-const handleChange = (person: Person) => {
-  setSelectedPeople(
-    findPerson(person) ? filterPerson(person) : [...selectedPeople, person]
-  );
-};
-const [selectedPeople, setSelectedPeople] = useState<Person[]>([]);
-const columns = [
-  {
-    field: "actions",
-    headerName: "Confirmed",
-    minWidth: 50,
-    renderCell: (params: GridRenderCellParams) => (
-      <Checkbox
-        size="small"
-        checked={findPerson(params.row.id)}
-        onChange={() => handleChange(params.row.id)}
-      />
-    ),
-  },
+export interface HomeInterface {}
 
-  {
-    field: "name",
-    headerName: "Name",
-    flex: 1,
-    minWidth: 150,
-    renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
-  },
-  {
-    field: "category",
-    headerName: "Categories",
-    flex: 1,
-    renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
-  },
-  {
-    field: "company",
-    headerName: "Company",
-    flex: 1,
-    renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
-  },
-];
+const Home: React.FC<HomeInterface> = () => {
+  const findPerson = (person: Person) =>
+    !!selectedPeople.find((p) => p.id === person.id);
+  const filterPerson = (person: Person) =>
+    selectedPeople.filter((p) => p.id === person.id);
+  const handleChange = (person: Person) => {
+    setSelectedPeople(
+      findPerson(person) ? filterPerson(person) : [...selectedPeople, person]
+    );
+  };
+  const [selectedPeople, setSelectedPeople] = useState<Person[]>([]);
+  const columns = [
+    {
+      field: "actions",
+      headerName: "Confirmed",
+      minWidth: 50,
+      renderCell: (params: GridRenderCellParams) => (
+        <Checkbox
+          size="small"
+          //     checked={findPerson(params.row.id)}
+          onChange={() => handleChange(params.row.id)}
+        />
+      ),
+    },
 
-const Home: React.FC<HomeProps> = () => {
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: "category",
+      headerName: "Categories",
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: "company",
+      headerName: "Company",
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+  ];
   return (
     <DataGrid
       rows={People}
@@ -67,5 +66,4 @@ const Home: React.FC<HomeProps> = () => {
     />
   );
 };
-
 export default Home;
