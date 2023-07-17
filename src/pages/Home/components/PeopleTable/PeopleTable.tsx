b44e3bf920138a7c729a6ea6d9a8fcfcd2be8a1b
gabrielagi <gabrielagi503@gmail.com>
2023-07-17 16:54:21 -0300

    Atomic Design -> Home a PeopleTable

--- src/pages/Home/components/PeopleTable/PeopleTable.tsx
+++ src/pages/Home/components/PeopleTable/PeopleTable.tsx
@@ -1,10 +1,80 @@
-import React from 'react';
+import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
+import { Person } from "@/models";
+import { Checkbox } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { addFavorites, addPeople } from "@/redux/states";
+import { AppStore } from "@/redux/store";
+import { useState } from "react";
 
-export type PeopleTableProps = {
-}
+export interface PeopleTableProps {}
 
-const PeopleTable: React.FC<PeopleTableProps>  = ({}) => {
-	return <div>PeopleTable</div>;
+const PeopleTable: React.FC<PeopleTableProps> = () => {
+  const dispatch = useDispatch();
+
+  const findPerson = (person: Person) =>
+    !!selectedPeople.find((p) => p.id === person.id);
+  const filterPerson = (person: Person) =>
+    selectedPeople.filter((p) => p.id !== person.id);
+  const handleChange = (person: Person) => {
+    const filteredPeople = findPerson(person)
+      ? filterPerson(person)
+      : [...selectedPeople, person];
+    dispatch(addFavorites(filteredPeople));
+    setSelectedPeople(filteredPeople);
+  };
+  const storePeople = useSelector((store: AppStore) => store.people);
+  const [selectedPeople, setSelectedPeople] = useState<Person[]>([]);
+  const columns = [
+    {
+      field: "actions",
+      type: "actions",
+      sortable: false,
+      headerName: "Confirmed",
+      minWidth: 50,
+      renderCell: (params: GridRenderCellParams) => (
+        <Checkbox
+          size="small"
+          checked={findPerson(params.row)}
+          onChange={() => handleChange(params.row)}
+        />
+      ),
+    },
+
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: "category",
+      headerName: "Categories",
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: "company",
+      headerName: "Company",
+      flex: 1,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+  ];
+
+  return (
+    <DataGrid
+      rows={storePeople}
+      columns={columns}
+      disableColumnSelector
+      disableRowSelectionOnClick
+      autoHeight
+      initialState={{
+        pagination: { paginationModel: { pageSize: 5 } },
+      }}
+      pageSizeOptions={[5, 10, 25]}
+      getRowId={(row: any) => row.id}
+    />
+  );
 };
 
 export default PeopleTable;
